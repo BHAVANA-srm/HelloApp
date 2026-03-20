36b26d9f942a8de50c8a416336d447094e782e84
bhavana-sys <rbhavana113@gmail.com>
2026-03-20 14:07:06 +0530

    Added HelloApp3

--- src/JAVA PRACTICE CODES/HelloApp3.java
+++ src/JAVA PRACTICE CODES/HelloApp3.java
@@ -1,13 +1,13 @@
-public class HelloApp {
+public class HelloApp3 {
     public static void main(String[] args) {
 
         // Check if an argument is provided
         if (args.length > 0) {
             String name = args[0];   // Read the first argument
             System.out.println("Hello, " + name + "!");
-        } 
+        }
         else {
-            System.out.println("Please provide a name as a command-line argument.");
+            System.out.println(" Hello World");
         }
     }
 }
